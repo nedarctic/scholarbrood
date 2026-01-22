652bb98a04fb6b28f66fc2267a3d313d786e4e91
justkimtai <justkimtai@gmail.com>
2026-01-22 23:04:44 +0300

    configured image domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // cacheComponents: true,
+  images: {
+    domains: ['scholarbrood.com'],
+  },
 };
 
 export default nextConfig;
